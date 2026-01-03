afd7ed2866ce988384adbffe78517e0a987957e6
lebuckman <liambuckman0@gmail.com>
2026-01-03 12:22:12 -0800

    feat: set up frontend with routes, clerk, tailwind
    - Update main entry point to include ClerkProvider and BrowserRouter
    - Set up routes for Home, Product, Profile, Create, and Edit pages
    - Created Navbar component with theme selector and auth ctas
    - Created working theme selector that saves selected themes based on DaiseyUI
    - Created placeholder pages for each route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import CreatePage from "./pages/CreatePage";
+import EditProductPage from "./pages/EditProductPage";
 
 function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    return (
+        <div className="min-h-screen bg-base-100">
+            <Navbar />
+            <main className="max-w-5xl mx-auto px-4 py-8">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/products/:id" element={<ProductPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route path="/create" element={<CreatePage />} />
+                    <Route path="/edit/:id" element={<EditProductPage />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
