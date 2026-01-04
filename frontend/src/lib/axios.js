712b7b8f75fd470a94c059face454d037490fd6b
lebuckman <liambuckman0@gmail.com>
2026-01-04 14:12:54 -0800

    feat: add env var check to axios config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+    throw new Error("Add API_URL to the .env file");
+}
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
     withCredentials: true, // send cookies with requests
 });
 
